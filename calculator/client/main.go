28b32c382725b2d8d8f82cff34590218255eee85
Dom Hallan <dom@domhallan.com>
2024-05-07 18:26:14 -0400

    Added gRPC client for calculator service
    A new gRPC client has been added for the calculator service. This includes setting up a connection to the server using insecure transport credentials, creating a new instance of the CalculatorServiceClient, and calling the doSum function. Error handling is also included in case the connection fails.

---
+++ calculator/client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/polyglotdev/grpc-masterclass/calculator/proto"
+)
+
+var addr = "localhost:50052"
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Failed to connect %v\n", err)
+	}
+
+	defer conn.Close()
+
+	c := pb.NewCalculatorServiceClient(conn)
+
+	doSum(c)
+}
